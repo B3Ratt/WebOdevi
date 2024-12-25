52452bd63c90c09c9c6bd990fad62e11c42735d8
B3Ratt <alpsar.berat03@hotmail.com>
2024-12-25 23:44:52 +0300

    admin işlemleri devam

--- WebOdevi/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebOdevi/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -86,16 +86,30 @@ namespace WebOdevi.Areas.Identity.Pages.Account
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 user.AdSoyad = Input.AdSoyad; // Yeni alanı kaydediyoruz
 
-                
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    // Kullanıcı kaydını başarıyla tamamladıktan sonra oturum açma
+                    // Kullanıcıya rol atama
+                    if (Input.Email == "b221210044@sakarya.edu.tr") // Admin rolü atanacak kullanıcı
+                    {
+                        if (!await _userManager.IsInRoleAsync(user, "Admin"))
+                        {
+                            await _userManager.AddToRoleAsync(user, "Admin");
+                        }
+                    }
+                    else // Diğer kullanıcılar "Müşteri" olarak atanacak
+                    {
+                        if (!await _userManager.IsInRoleAsync(user, "Müşteri"))
+                        {
+                            await _userManager.AddToRoleAsync(user, "Müşteri");
+                        }
+                    }
+
                     await _signInManager.SignInAsync(user, isPersistent: false);
-                    return LocalRedirect(returnUrl); // Kullanıcı doğrudan giriş yapacak ve yönlendirilecek
+                    return LocalRedirect(returnUrl);
                 }
 
                 foreach (var error in result.Errors)
@@ -104,9 +118,9 @@ namespace WebOdevi.Areas.Identity.Pages.Account
                 }
             }
 
-            // Eğer burada bir hata oluşursa, form tekrar gösterilecek
             return Page();
         }
+
 
         private Kullanici CreateUser()
         {
--- WebOdevi/Migrations/20241225202430_rol.Designer.cs
+++ WebOdevi/Migrations/20241225202430_rol.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebOdevi.Models;
 
@@ -11,9 +12,11 @@ using WebOdevi.Models;
 namespace WebOdevi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241225202430_rol")]
+    partial class rol
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
